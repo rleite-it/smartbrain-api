9410be8dca461d75f43407450eae0616d744435e
Ricardo Leite <blackstormofmilk@gmail.com>
2022-01-29 20:16:22 +0000

    Change listening port

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.put("/image", (req, res) => {
 // IMAGE URL
 app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is runnin on port ${process.env.PORT}`);
 });
